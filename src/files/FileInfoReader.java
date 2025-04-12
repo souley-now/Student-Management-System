a6a2c51be34b0f69e434739d68bf670929b6d0b4
Souley Diallo <diallogs@yahoo.com>
2025-04-12 14:38:30 -0400

    Remove unnecessary configuration and data files from the project

--- src/files/FileInfoReader.java
+++ src/files/FileInfoReader.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class `FileInfoReader {
+public class FileInfoReader {
     private List<Course> courses = new ArrayList<>();
     private List<Admin> admins = new ArrayList<>();
     private List<Student> students = new ArrayList<>();
